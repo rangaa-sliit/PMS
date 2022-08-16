7bf4ce3dd7a3fddb32915fb22a3f144c34b25ac1
Ranga Athapaththu <ranga.a@sliit.lk>
2022-08-16 20:51:17 +0530

    Initial Commit

---
+++ PMS/PMS/App_Start/BundleConfig.cs
@@ -0,0 +1,53 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace PMS
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-3.6.0.slim.min.js",
+                        "~/Scripts/jquery-3.6.0.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate.min.js",
+                        "~/Scripts/jquery.validate.unobtrusive.min.js"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-2.8.3.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.min.js",
+                      "~/Scripts/sb-admin-2.min.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/Site.css",
+                      "~/Content/bootstrap.min.css",
+                      "~/Content/font-family-nunito.css",
+                      "~/Content/sb-admin-2.min.css"));
+
+            bundles.Add(new ScriptBundle("~/DataTable/css").Include(
+                      "~/Content/dataTables.bootstrap4.min.css",
+                      "~/Content/buttons.bootstrap4.min.css"));
+
+            bundles.Add(new ScriptBundle("~/DataTable/js").Include(
+                      "~/Scripts/jquery.dataTables.min.js",
+                      "~/Scripts/dataTables.bootstrap4.min.js",
+                      "~/Scripts/dataTables.buttons.min.js",
+                      "~/Scripts/buttons.bootstrap4.min.js",
+                      "~/Scripts/buttons.print.min.js",
+                      "~/Scripts/buttons.html5.min.js"));
+
+            bundles.Add(new ScriptBundle("~/Popper/js").Include(
+                      "~/Scripts/popper-4.0.min.js"));
+
+            bundles.Add(new ScriptBundle("~/Notify/js").Include(
+                      "~/Scripts/notify.min.js"));
+        }
+    }
+}
---
+++ PMS/PMS/Controllers/SAController.cs
@@ -0,0 +1,135 @@
+﻿using PMS.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PMS.Controllers
+{
+    public class SAController : Controller
+    {
+        // GET: SA
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ManageInstitutes()
+        {
+            return View();
+        }
+
+        public ActionResult GetInstitutes()
+        {
+            using (PMSEntities db = new PMSEntities())
+            {
+                List<Institute> InstituteList = (from i in db.Institute orderby i.InstituteId descending select i).ToList();
+                return Json(new { data = InstituteList }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        [HttpGet]
+        public ActionResult AddOrEditInstitute(int id = 0)
+        {
+            if(id == 0)
+            {
+                return View(new Institute());
+            }
+            else
+            {
+                using (PMSEntities db = new PMSEntities())
+                {
+                    return View((from i in db.Institute where i.InstituteId.Equals(id) select i).FirstOrDefault<Institute>());
+                }
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult AddOrEditInstitute(Institute institute)
+        {
+            using (PMSEntities db = new PMSEntities())
+            {
+                try
+                {
+                    var dateTime = DateTime.Now;
+                    if (institute.InstituteId == 0)
+                    {
+                        Institute validationRecord = (from i in db.Institute where i.InstituteCode.Equals(institute.InstituteCode) && i.InstituteName.Equals(institute.InstituteName) select i).FirstOrDefault<Institute>();
+                        if (validationRecord != null)
+                        {
+                            return Json(new
+                            {
+                                success = false,
+                                message = "This Institute Already Exists"
+                            }, JsonRequestBehavior.AllowGet);
+                        }
+                        else
+                        {
+                            institute.CreatedBy = "Ranga";
+                            institute.CreatedDate = dateTime;
+                            institute.ModifiedBy = "Ranga";
+                            institute.ModifiedDate = dateTime;
+
+                            db.Institute.Add(institute);
+                            db.SaveChanges();
+
+                            return Json(new
+                            {
+                                success = true,
+                                message = "Successfully Saved"
+                            }, JsonRequestBehavior.AllowGet);
+                        }
+                    }
+                    else
+                    {
+                        Institute editingInstitute = (from i in db.Institute where i.InstituteId.Equals(institute.InstituteId) select i).FirstOrDefault<Institute>();
+
+                        if (editingInstitute.InstituteCode != institute.InstituteCode || editingInstitute.InstituteName != institute.InstituteName || editingInstitute.IsActive != institute.IsActive)
+                        {
+                            editingInstitute.InstituteCode = institute.InstituteCode;
+                            editingInstitute.InstituteName = institute.InstituteName;
+                            editingInstitute.IsActive = institute.IsActive;
+                            editingInstitute.ModifiedBy = "Ranga";
+                            editingInstitute.ModifiedDate = dateTime;
+
+                            db.Entry(editingInstitute).State = EntityState.Modified;
+                            db.SaveChanges();
+
+                            return Json(new
+                            {
+                                success = true,
+                                message = "Successfully Updated"
+                            }, JsonRequestBehavior.AllowGet);
+                        }
+                        else
+                        {
+                            return Json(new
+                            {
+                                success = false,
+                                message = "You didn't make any new changes"
+                            }, JsonRequestBehavior.AllowGet);
+                        }
+                    }
+                }
+                catch (System.Data.Entity.Validation.DbEntityValidationException dbEx)
+                {
+                    Exception raise = dbEx;
+                    foreach (var validationErrors in dbEx.EntityValidationErrors)
+                    {
+                        foreach (var validationError in validationErrors.ValidationErrors)
+                        {
+                            string message = string.Format("{0}:{1}",
+                                validationErrors.Entry.Entity.ToString(),
+                                validationError.ErrorMessage);
+                            raise = new InvalidOperationException(message, raise);
+                        }
+                    }
+                    throw raise;
+                }
+            }
+        }
+    }
+}
